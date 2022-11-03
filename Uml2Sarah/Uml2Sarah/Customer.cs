dd4751239f656de70232d32d3b01f282ee2a9000
zenia2000 <77238819+zenia2000@users.noreply.github.com>
2022-11-03 15:06:26 +0100

    Add files via upload

---
+++ Uml2Sarah/Uml2Sarah/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//setup for one customer
+
+namespace Uml2Sarah
+{
+    public class Customer
+    {
+        string _id;
+        string _name;
+        string _phone;
+        string _address;
+
+        public Customer(string id, string name, string phone, string address)
+        {
+            _id = id;
+            _name = name;
+            _phone = phone;
+            _address = address;
+        }
+        public string Id { get { return _id; } }
+        public string Name { get { return _name; } }
+        public string Phone { get { return _phone; } }
+        public string Address { get { return _address; } }
+
+        public override string ToString()
+        {
+            return $"Customer no. {_id}, {_name}. Phone: {_phone}. Address: {_address}";
+        }
+    }
+}
---
+++ Uml2Sarah/Uml2Sarah/PizzaLibrary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Info on all pizzas
+
+namespace Uml2Sarah
+{
+    public class PizzaLibrary
+    {
+
+        private List<Pizza> _pizzaList;
+        public PizzaLibrary()
+        {
+            _pizzaList = new List<Pizza>();
+        }
+        public int Count
+        { 
+            get { return _pizzaList.Count; } 
+        }
+
+        public void PrintPizza()
+        {
+            foreach (Pizza pizza in _pizzaList)
+            {
+                Console.WriteLine(pizza.ToString());
+            }
+        }
+
+        public void AddPizza(Pizza aPizza)
+        {
+            
+            _pizzaList.Add(aPizza);
+        }
+
+        public Pizza LookupPizza(string pizzaNo)
+        {
+            foreach (Pizza item in _pizzaList)
+            {
+                if (item.PizzaNo == pizzaNo)
+                    return item;
+            }
+            return null;
+        }
+        public void DeletePizza(string pizzaNo)
+        {
+            _pizzaList.Remove(LookupPizza(pizzaNo));
+        }
+
+        public void EditPizza(string pizzaNo, Pizza pizzaToEdit)
+        {
+            int a = 0;
+            while (a < _pizzaList.Count)
+            {
+                if (_pizzaList[a].PizzaNo == pizzaNo)
+                {
+                    _pizzaList[a] = pizzaToEdit;
+                    break;
+                }
+                a++;
+            }
+        }
+    }
+}
